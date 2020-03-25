7ff5695aaefab8ef9a4391fd627d631b9c5122f3
tworrell <tara.worrell@gmail.com>
2020-03-24 22:29:23 -0400

    updated code will be adding more functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class TodoList extends Component {
+  constructor() {
+    super();
+    this.state ={
+      todos: [],
+      currentTodo: '',
+    };
+  }
+
+//this is where our methods will go
+  handleChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  addItem = (event) => {
+    event.preventDefault();
+    console.log('the add item method executed');
+  }
+
+
+
+  render() {
+    return(
+      <div className='container'>
+        <form onSubmit={this.addItem}>
+          <label htmlFor ='taskName'>Task Name:</label>
+          <input onChange={this.handleChange} name='taskName' placeholder='Place your ToDo here'/>
+          <button type='submit'>Add Task</button>
+        </form>
+        <ul>
+        {
+                this.state.todos.map((q, key) => {
+                    return(
+                        <li key={key}>{q}</li>
+                    )
+                })
+            }
+          //Here is where I want to map through the todo list 
+        </ul>
+      </div>
+    )
+  };
 }
 
-export default App;
+export default TodoList;
